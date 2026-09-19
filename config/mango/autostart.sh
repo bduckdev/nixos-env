7c3952a09f31e46338db402ca182a4caf6d81924
Brennan Duck <brennantduck@gmail.com>
2026-09-19 15:37:53 -0400

    mango: updated autostart

--- config/mango/autostart.sh
+++ config/mango/autostart.sh
@@ -1,16 +1,18 @@
-  #!/usr/bin/env sh
-  set -eu
+#!/usr/bin/env sh
+set -eu
 
-  dbus-update-activation-environment --systemd \
-    DISPLAY \
-    WAYLAND_DISPLAY \
-    XDG_CURRENT_DESKTOP \
-    XDG_SESSION_TYPE \
-    XCURSOR_THEME \
-    XCURSOR_SIZE \
-    MANGO_INSTANCE_SIGNATURE
+dbus-update-activation-environment --systemd \
+  DISPLAY \
+  WAYLAND_DISPLAY \
+  XDG_CURRENT_DESKTOP \
+  XDG_SESSION_TYPE \
+  XCURSOR_THEME \
+  XCURSOR_SIZE \
+  MANGO_INSTANCE_SIGNATURE
 
-  systemctl --user reset-failed
-  systemctl --user start mango-session.target
+systemctl --user reset-failed
+systemctl --user start mango-session.target
 
 
+noctalia & 
+tmux new-session -d -s "main" &
