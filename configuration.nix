566012762507e26e08c110901063caa292cec373
Brennan Duck <brennantduck@gmail.com>
2026-09-19 15:39:01 -0400

    core: added man pages

--- configuration.nix
+++ configuration.nix
@@ -14,6 +14,15 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  documentation.man.enable = true;
+
+  hardware = {
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+  };
+
   systemd.user.targets.mango-session = {
     description = "Mango compositor session";
     documentation = [ "man:systemd.special(7)" ];
@@ -24,15 +33,24 @@
 
   networking = {
     hostName = "nixos-desktop";
-    networkmanager.enable = true;
-    interfaces.enp7s0.ipv4.addresses = [
-      {
-        address = "192.168.1.69";
-        prefixLength = 24;
-      }
-    ];
-
-    defaultGateway = "192.168.1.1";
+    networkmanager = {
+      enable = true;
+      settings.main.no-auto-default = "*";
+      ensureProfiles.profiles.enp7s0 = {
+        connection = {
+          id = "enp7s0";
+          type = "ethernet";
+          interface-name = "enp7s0";
+          autoconnect = true;
+        };
+        ipv4 = {
+          method = "manual";
+          addresses = "192.168.1.69/24";
+          gateway = "192.168.1.1";
+        };
+        ipv6.method = "link-local";
+      };
+    };
 
     nameservers = [
       "192.168.1.1"
@@ -55,15 +73,69 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  services = {
-    xserver = {
+  security.rtkit.enable = true;
+
+  users.users.bduck = {
+    isNormalUser = true;
+    description = "Brennan Duck";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+    shell = pkgs.zsh;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFt8/My570WXRzBbQi5LNMX7g0Srsw9y+Vjcc1Yj0P0r bduck@continuumcloud.com"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFXlT1LZafXku9iQAeXMacUwl3A8l1cMBLUIWZ5xtanX"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINZ4rzsuIDoo/u5X89OShjMZ1fSH5o12gMrBwYyKAG+X brennantduck@gmail.com"
+    ];
+  };
+
+  programs = {
+    dconf.enable = true;
+    gamemode.enable = true;
+    gamescope = {
       enable = true;
-      xkb = {
-        layout = "us";
-        variant = "";
+      #capSysNice = true;
+      enableWsi = true;
+    };
+    mango.enable = true;
+    noctalia.enable = true;
+    steam = {
+      enable = true;
+      gamescopeSession = {
+        enable = true;
+        args = [
+          "--hdr-enabled"
+          "--adaptive-sync"
+        ];
+        steamArgs = [
+          "-gamepadui"
+          #"-tenfoot"
+          "-pipewire-dmabuf"
+        ];
       };
     };
+    zsh.enable = true;
+  };
 
+  environment = {
+    localBinInPath = true;
+
+    sessionVariables = {
+      NH_FLAKE = "/home/bduck/nixos-env/";
+    };
+
+    systemPackages = [
+      inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
+      pkgs.gnomeExtensions.focus-changer
+      pkgs.mangohud
+      pkgs.wl-clipboard
+      pkgs.wl-clip-persist
+    ];
+
+  };
+
+  services = {
     openssh = {
       enable = true;
       openFirewall = true;
@@ -73,6 +145,16 @@
         KbdInteractiveAuthentication = false;
         PermitRootLogin = "no";
       };
+    };
+
+    xserver = {
+      enable = true;
+      videoDrivers = [ "amdgpu" ];
+      xkb = {
+        layout = "us";
+        variant = "";
+      };
+
     };
 
     displayManager.gdm.enable = true;
@@ -92,42 +174,6 @@
     };
   };
 
-  security.rtkit.enable = true;
-
-  users.users.bduck = {
-    isNormalUser = true;
-    description = "Brennan Duck";
-    extraGroups = [
-      "networkmanager"
-      "wheel"
-    ];
-    shell = pkgs.zsh;
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFt8/My570WXRzBbQi5LNMX7g0Srsw9y+Vjcc1Yj0P0r bduck@continuumcloud.com"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFXlT1LZafXku9iQAeXMacUwl3A8l1cMBLUIWZ5xtanX"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINZ4rzsuIDoo/u5X89OShjMZ1fSH5o12gMrBwYyKAG+X brennantduck@gmail.com"
-    ];
-  };
-
-  programs = {
-    mango.enable = true;
-    noctalia.enable = true;
-    steam.enable = true;
-    zsh.enable = true;
-  };
-
-  environment = {
-    localBinInPath = true;
-
-    systemPackages = [
-      inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
-      pkgs.gnomeExtensions.focus-changer
-      pkgs.wl-clipboard
-      pkgs.wl-clip-persist
-    ];
-
-  };
-
   nixpkgs.config.allowUnfree = true;
   nix.settings.experimental-features = [
     "nix-command"
@@ -138,18 +184,16 @@
     pkgs.nerd-fonts.jetbrains-mono
   ];
 
+  systemd.sleep.extraConfig = ''
 
+    AllowSuspend=no
 
-systemd.sleep.extraConfig = ''
+    AllowHibernation=no
 
-  AllowSuspend=no
+    AllowHybridSleep=no
 
-  AllowHibernation=no
+    AllowSuspendThenHibernate=no
 
-  AllowHybridSleep=no
-
-  AllowSuspendThenHibernate=no
-
-'';
+  '';
   system.stateVersion = "26.05"; # Keep the version from the first install.
 }
